463275dba331afaf152b4c8969e0858d60f0d7ee
Dmitry Ryumkin <riumkin@gmail.com>
2018-01-23 14:07:15 +0530

    add header to screen

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Text, AppRegistry } from 'react-native'
+import { AppRegistry } from 'react-native'
+import Header from './src/components/header'
 
+
+// Create a component
 const App = () => (
-  <Text>Some Text</Text>
+  <Header headerText={'Albums'} />
 )
 
+// Render it to the device
 AppRegistry.registerComponent('albums', () => App)
