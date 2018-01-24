340d88d6787790e6d2f826b22567d691e29f719c
Dmitry Ryumkin <riumkin@gmail.com>
2018-01-24 16:21:12 +0530

    implement album art

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,22 +4,34 @@ import Card from './Card'
 import CardSection from './CardSection'
 
 const AlbumDetail = ({ data }) => {
-  const { title, artist, thumbnail_image } = data
-  const { thumbnailStyle, headerContentStyle } = styles
+  const { title, artist, thumbnail_image, image } = data
+  const { 
+    thumbnailStyle,
+    headerContentStyle,
+    thumnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles
 
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumnailContainerStyle}>
           <Image 
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }} 
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image 
+          style={imageStyle}
+          source={{ uri: image }} 
+        />
       </CardSection>
     </Card>
   )
@@ -30,9 +42,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
